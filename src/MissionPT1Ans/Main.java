e405a2d529acaa92e4376b3923f08867f7dc6b88
MasahiroSuzuki86 <tobetoeurabemasa@gmail.com>
2023-10-12 08:35:53 +0900

    テスト

--- src/MissionPT1Ans/Main.java
+++ src/MissionPT1Ans/Main.java
@@ -7,7 +7,25 @@ package MissionPT1Ans;
  */
 public class Main {
 	public static void main(String[] args) {
+		Warrior yositaka = new Warrior();
+		yositaka.setAp(40);
+		yositaka.setDp(40);
+		yositaka.setHp(1000);
+		yositaka.setLuck(10);
+		yositaka.setMp(0);
+		yositaka.setName("よしたか");
 		
+		System.out.println(yositaka.getName());
+		yositaka.setName("スーパーよしたか");
+		System.out.println(yositaka.getName());
+		
+		
+		Warrior takeshi = new Warrior();
+		takeshi.setDp(5);
+		takeshi.setHp(100);
+		takeshi.setName("たけし");
+		
+		yositaka.attack(takeshi);
 	}
 
 }
